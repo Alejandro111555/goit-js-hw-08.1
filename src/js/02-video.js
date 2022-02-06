a5c8865cbcd3c07bbc03ea51ff1f09752a2d271a
alejandro111555 <al.mirskoy@gmail.com>
2022-02-06 13:16:53 +0200

    completed task two

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,39 @@
+// Ознакомься с документацией библиотеки Vimeo плеера.
+// Добавь библиотеку как зависимость проекта через npm.
+// Инициализируй плеер в файле скрипта как это описано в секции pre-existing player, но учти что у тебя плеер добавлен как npm пакет, а не через CDN.
+// Разбери документацию метода on() и начни отслеживать событие timeupdate - обновление времени воспроизведения.
+// Сохраняй время воспроизведения в локальное хранилище. Пусть ключом для хранилища будет строка "videoplayer-current-time".
+// При перезагрузке страницы воспользуйся методом setCurrentTime() для того чтобы возобновить воспроизведение с сохраненной позиции.
+// Добавь в проект бибилотеку lodash.throttle и сделай так, чтобы время воспроизведения обновлялось в хранилище не чаще чем раз в секунду.
 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe#vimeo-player');
+const player = new Vimeo.Player(iframe);
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+let currentTime = localStorage.getItem(LOCALSTORAGE_KEY);
+
+function getCurrentVideo(data) {
+  let timeInSeconds = data.seconds;
+  localStorage.setItem(LOCALSTORAGE_KEY, timeInSeconds);
+}
+
+player.on('timeupdate', throttle(getCurrentVideo, 1000));
+
+player
+  .setCurrentTime(currentTime)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
